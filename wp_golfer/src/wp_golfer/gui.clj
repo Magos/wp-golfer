77cd3833f2c50e3a47aaea18075d648bd0d0337b
Magnus Grimstvedt Saltnes <magnussaltnes@gmail.com>
2013-01-27 15:55:18 +0100

    GUI reimplemented with Seesaw.

--- wp_golfer/src/wp_golfer/gui.clj
+++ wp_golfer/src/wp_golfer/gui.clj
@@ -1,62 +1,29 @@
-(ns wp-golfer.gui (:use wp-golfer.core)
-  (:import (javax.swing JFrame JPanel JButton JLabel JTextField JList event.DocumentListener) )
+(ns wp-golfer.gui (:use wp-golfer.core seesaw.core)
+  (:import (javax.swing JTextField ) )
   )
-
-(defmacro set-grid! [constraints field value]
-  `(set! (. ~constraints ~(symbol (name field)))
-         ~(if (keyword? value)
-            `(. java.awt.GridBagConstraints
-                ~(symbol (name value)))
-            value)))
-
-(defmacro grid-bag-layout [container & body]
-  (let [c (gensym "c")
-        cntr (gensym "cntr")]
-    `(let [~c (new java.awt.GridBagConstraints)
-           ~cntr ~container]
-       ~@(loop [result '() body body]
-           (if (empty? body)
-             (reverse result)
-             (let [expr (first body)]
-               (if (keyword? expr)
-                 (recur (cons `(set-grid! ~c ~expr
-                                          ~(second body))
-                              result)
-                        (next (next body)))
-                 (recur (cons `(.add ~cntr ~expr ~c)
-                              result)
-                        (next body)))))))))
-
-(defn gui []
-  (let [
-        tee-field (JTextField. 15)
-        hole-field (JTextField. 15)
-        output (JLabel. "Lorem ipsum dolor sit amet, consectetur adicisiping elit.")
-        update (fn [] (.setText output (golf (.getText tee-field) (.getText hole-field))))
-        panel (doto (JPanel.)
-                (grid-bag-layout 
-                 :fill :BOTH, :insets ;(Insets. 5 5 5 5),
-                 :gridx 0, :gridy 0
-                 (JLabel. "Tee:")
-                 :gridx 1
-                 tee-field
-                 :gridx 2
-                 (JLabel. "Hole:")
-                 :gridx 3
-                 hole-field
-                 :gridx 0, :gridy 1, :gridwidth 2
-                 output
-                 )
-                )
+(defn golf-gui []
+  (native!)
+  (let [tee-lbl "Tee:"
+        hole-lbl "Hole:"
+        tee (JTextField. 15)
+        hole (JTextField. 15)
+        storage (atom "Output." :validator-fn string?)
+        label (javax.swing.JLabel. @storage)
+        do-search (fn [x] (golf (.getText tee) (.getText hole)))
+        update (fn [evt] (invoke-later (swap! storage do-search) (.setText label (str @storage))))
+        update-action (action :handler update :name "Search")
+        pan (flow-panel 
+             :align :left
+             :hgap 20
+             :items [tee-lbl tee hole-lbl hole update-action label]
+             )
         ]
-    ;;(.addDocumentListener tee-field (proxy ))
-    (doto (JFrame. "Test")
-      (.add panel)
-      (.pack)
-      (.setVisible true)
-      (.setDefaultCloseOperation (. JFrame DISPOSE_ON_CLOSE))
-      )
+    ;(listen tee :key update)
+    ;(listen hole :key update)
+    (doto (frame :content pan :on-close :dispose)
+      (pack!)
+      (show!))
     )
   )
 
-;(gui)
+(golf-gui)
