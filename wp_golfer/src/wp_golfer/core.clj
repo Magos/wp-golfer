31ab7a3bac38e954ce0883683c10de2cc5ea66d2
Magnus Grimstvedt Saltnes <magnussaltnes@gmail.com>
2013-02-22 15:21:37 +0100

    Rewrote with bidirectional search.

--- wp_golfer/src/wp_golfer/core.clj
+++ wp_golfer/src/wp_golfer/core.clj
@@ -1,61 +1,53 @@
 (ns wp-golfer.core (:require [wp-golfer.retrieval :as re]))
 (def ^:dynamic *depth-limit* 200)
 
-(defn search [frontier parents finish depth]
-  ;;If map contains finish
-  (if (< *depth-limit* depth)
-    :depth-limit-exceeded
-   (if (contains? parents finish)
-    ;;then return it.
-    parents
-    ;;else:
-    (let [;;Pop first member off queue
-          article (nth frontier 0)
-          ;;Get new child articles.
-          not-in-parents (fn [x] (not (contains? parents x)))
-          children (filter not-in-parents (re/child-articles article))
-          ;;Add each to map and seq if not found in map already.
-          nFrontier (drop 1 (into frontier children))
-          nParents (conj parents (zipmap children (repeat article)))
-          ;;Recur with smaller frontier, larger parent map.
+(defn search 
+  "Perform bi-directional search."
+  [f-frontier b-frontier f-parents b-parents]
+  (if (some (partial contains? f-parents) (keys b-parents))
+    {:forward f-parents :backward b-parents}
+    (let [fwd-article (first f-frontier)
+          bck-article (first b-frontier)
+          not-in-f (comp not (partial contains? f-parents))
+          not-in-b (comp not (partial contains? b-parents))
+          fwd-children (filter not-in-f (wp-golfer.retrieval/child-articles fwd-article))
+          bck-parents (filter not-in-b (wp-golfer.retrieval/parent-articles bck-article))
+          nf-parents (into f-parents (zipmap fwd-children (repeat fwd-article)))
+          nb-parents (into b-parents (zipmap bck-parents (repeat bck-article)))
+          nf-frontier (drop 1 (into f-frontier fwd-children))
+          nb-frontier (drop 1 (into b-frontier bck-parents))
           ]
-      ;;nil
-      (recur nFrontier nParents finish (inc depth))
+      (recur nf-frontier nb-frontier nf-parents nb-parents)
       )
     )
-   )
   )
-           
-
-(defn reconstruct-path [parents finish]
-  (let [r1 (fn [path parents]
-             (if (some (fn [x] (= :start x)) path)
-               path
-               (let [node (peek path)
-                     parent (get parents node)
-                     nPath (conj path parent)]
-                 (recur nPath parents)
-                 )
-               ))
-        result (r1 (vector finish) parents)]
-    (pop (reverse result))
+  
+  (defn reconstruct-path [{:keys [forward backward]}]
+    (let [overlap (some (set (keys forward)) (keys backward))
+          forward-path (take-while #(not= :start %1) (iterate #(get forward %1) overlap))
+          backward-path (take-while #(not= :finish %1) (iterate #(get backward %1) overlap))
+          ]
+      (concat (reverse (drop 1 forward-path)) backward-path)
+      )
     )
-  )
-
-        
+  
+  
+  
 (defn golf [start finish] 
-  ;;Naive forward breadth-first, to start with.
+  ;;Bi-directional breadth-first search.
   ;;Create frontier (things to search) as vector.
   ;;Create map of parents.
   ;;Populate.
-  (let [frontier (vector start)
-        parents (hash-map start :start)
+  (let [ffrontier [start]
+        bfrontier [finish]
+        fparents {start :start}
+        bparents {finish :finish}
         ;;Perform search.
-        result (search frontier parents finish 0)]
+        result (search ffrontier bfrontier fparents bparents)]
   ;;Reconstruct path using resulting parent-map and return.
     (if (= :depth-limit-exceeded result)
       (str result)
-     (reconstruct-path result finish)
+     (reconstruct-path result)
      )
     )
   )
