39fac6a03d1093c1f7c5b258160def1f80d94c37
Magnus Grimstvedt Saltnes <magnussaltnes@gmail.com>
2013-03-04 10:13:20 +0100

    Removed the descriptive text in the text areas at start. Added listeners to update output on hitting return.

--- wp_golfer/src/wp_golfer/gui.clj
+++ wp_golfer/src/wp_golfer/gui.clj
@@ -1,34 +1,45 @@
 (ns wp-golfer.gui (:use wp-golfer.core seesaw.core)
   (:require [seesaw.bind :as bind] [seesaw.mig])
   )
-(def targets (atom ()))
+(def targets (atom () :validator seq?))
 
 
 (defn golf-panel[]
   (let[tee (text :columns 10)
        hole (text :columns 10)
-       btn (button :text "Search")
-       output (label "")
-       update (fn [e] (let[result (wp-golfer.core/golf (first @targets) (last @targets))]
-                        (text! output (apply str (interpose " " result)))
+       tbl (table :minimum-size [300 :by 125]
+                  :model [:columns [:article]
+                          :rows nil
+                          ]
+                  )
+       update (fn [e] (let[result (golf (first @targets) (last @targets))]
+                        (config! tbl :model [:columns [:article]
+                                             :rows (map vector result)])
                         )
                 )
+       btn (button :action (action :handler update
+                                   :name "Search"
+                                   :tip "Perform a search")
+                   )
        ]
     (bind/bind
      (bind/funnel tee hole)
      (bind/transform (partial map #(.replaceAll % " " "_")))
      targets
      )
-    (listen btn :action update)
-    (seesaw.mig/mig-panel :items [[tee ""][hole ""] [btn "wrap"] [output "span"]])
+    (listen tee
+            :action update)
+    (listen hole 
+            :action update)
+    (seesaw.mig/mig-panel :items [[tee ""][hole ""] [btn "wrap"] [tbl "span"]])
     )
   )
 
 (defn gui[]
   (let [pnl (golf-panel)]
-  (-> (frame :title "Wikipedia Golfer" :content pnl)
-      pack!
-      show!
-   )
-  )
+    (-> (frame :title "Wikipedia Golfer" :content pnl :on-close :dispose)
+        pack!
+        show!
+        )
+    )
   )
